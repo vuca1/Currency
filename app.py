c67f136c48e38b575170ea64c160c53f58e23b74
vuca1 <malihavita@gmail.com>
2025-05-06 08:25:21 +0200

    added GUI with list menus and button to refresh labels [rate to 1, and final rates]

--- app.py
+++ app.py
@@ -1,19 +1,64 @@
 import requests
+import tkinter as tk
+from tkinter import ttk
 
-from_currency = input("Input base currency: ")
-from_currency = from_currency.upper()
-amount = input("Input amount:")
-to_currency = input("Input final currency: ")
-to_currency = to_currency.upper()
+def main():
 
-print(from_currency + to_currency)
+    #main window
+    window = tk.Tk()
+    window.title("Currency Calculater")
+    window.geometry("300x200")
+    window.resizable(False, False)
+
+    # drop down options
+    currencies = [
+        "JPY", "CZK", "EUR", "USD"
+    ]
+
+    # dropdown menu
+    currencies_menu_from = ttk.Combobox(window, values=currencies)
+    currencies_menu_from.set("Select base currency:")
+    currencies_menu_from.pack(pady=5)
+
+    currencies_menu_to = ttk.Combobox(window, values=currencies)
+    currencies_menu_to.set("Select final currency:")
+    currencies_menu_to.pack(pady=5)
+
+    # information entry
+    amount_entry = tk.Entry(window, width=15)
+    amount_entry.pack(pady=5)
+
+    def update_output():
+        currency_from = currencies_menu_from.get()
+        currency_to = currencies_menu_to.get()
+        amount = amount_entry.get()
+        print(currency_from + currency_to + amount_entry.get())
+        response = requests.get(f"https://api.frankfurter.app/latest?amount={amount}&from={currency_from}&to={currency_to}")
+
+        try:
+            response.raise_for_status() == 200
+            data = response.json()
+            final_rates = float(data["rates"][currency_to])
+            rate = f"1 {currency_from} = {final_rates/float(amount)} {currency_to}"
+            rates_to_1_label.config(text=rate)
+            final_rates_label.config(text=(f"{amount} {currency_from} = {str(final_rates)} {currency_to}"))
+            print(data)
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+
+    # button to trigger update_output
+    refresh_button = tk.Button(window, text="Calculate", command=update_output)
+    refresh_button.pack(pady=5)
+
+    # output data label
+    rates_to_1_label = tk.Label(window, text="")
+    rates_to_1_label.pack(pady=5)
+
+    final_rates_label = tk.Label(window, text="")
+    final_rates_label.pack(pady=5)
 
 
-response = requests.get(f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}")
+    window.mainloop()
 
-try:
-    response.raise_for_status() == 200
-    data = response.json()
-    print(data)
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+if __name__ == "__main__":
+    main()
